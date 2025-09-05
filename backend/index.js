f2890280044c33d6293e58674d118d574ab54b23
KSVenkatesh2002 <kotavenkatesh2002@gmail.com>
2025-09-05 19:31:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,15 +17,14 @@ app.use(cookieParser());
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
-})
-);
+}));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")))
   
-  app.get('*', (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  })
+  });
 }
 
 server.listen(PORT, () => {
